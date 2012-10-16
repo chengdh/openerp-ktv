acf2c196064928cb7bd17d36b592fde8a5762336
chengdh <cheng.donghui@gmail.com>
2012-10-17 00:07:21 +0800

    添加了包厢打折信息设置功能

--- openerp/addons/ktv_sale/ktv_room.py
+++ openerp/addons/ktv_sale/ktv_room.py
@@ -97,7 +97,7 @@ class ktv_room_type(osv.osv):
             #酒水价格
             #TODO
             #计费方式
-            'fee_type_id' : fields.many2one('ktv.ktv_fee_type',"ktv_fee_type"),
+            'fee_type_id' : fields.many2one('ktv.ktv_fee_type',"fee_type_id"),
             #容纳人数
             'serve_persons' : fields.integer('serve_persons'),
             #包厢费
@@ -181,4 +181,113 @@ class ktv_pay_type(osv.osv):
             }
 
 ktv_pay_type()
+
+class room_discount(osv.osv):
+    """包厢打折设置,设置包厢每周各天的钟点费打折情况"""
+    _name = "ktv.room_discount"
+    _discription = "包厢打折设置,设置包厢每周各天的钟点费打折情况"
+    #费用字段定义
+    #其中discount字段存储的是百分比
+    _fee_fields = [
+            "mon_hourly_fee",
+            "mon_hourly_discount",
+            "mon_continue_hourly_fee",
+            #周二
+            "tue_hourly_fee",
+            "tue_hourly_discount",
+            "tue_continue_hourly_fee",
+            #周三
+            "wed_hourly_fee",
+            "wed_hourly_discount",
+            "wed_continue_hourly_fee",
+            #周四
+            "thurs_hourly_fee",
+            "thurs_hourly_discount",
+            "thurs_continue_hourly_fee",
+            #周五
+            "fri_hourly_fee",
+            "fri_hourly_discount",
+            "fri_continue_hourly_fee",
+            #周六
+            "sat_hourly_fee",
+            "sat_hourly_discount",
+            "sat_continue_hourly_fee",
+            #周日
+            "sun_hourly_fee",
+            "sun_hourly_discount",
+            "sun_continue_hourly_fee",
+            #特殊日
+            "special_day_hourly_fee",
+            "special_day_hourly_discount",
+            "special_day_continue_hourly_fee",
+            ]
+
+    _columns = { field_name : fields.float(field_name,digits =(15,5)) for field_name in _fee_fields}
+
+    _columns.update({
+            #包厢类别
+            "room_type_id" : fields.many2one("ktv.ktv_room_type","room_type_id",required = True),
+
+            #FIXME 此处注意:起始时间和结束时间指的是消费时长,消费时长越多,折扣越大,也是鼓励客人消费的一种手段
+            #打折消费时间起始值(以半小时作为计算单位)
+            #采用 <消费时长<= 方式来比对打折费用
+            "consume_time_gt": fields.integer("consume_time_gt",required = True),
+            #打折消费时间结束值
+            "consume_time_lte": fields.integer("consume_time_lte",required = True),
+            #打折参考的基准价格,默认等于该包厢类别room_type的钟点费,用户可以修改
+            "base_hourly_fee" : fields.float("base_hourly_fee",digits =(15,5)),
+            })
+
+    _defaults = { field_name : 0 for field_name in _fee_fields}
+
+    _defaults.update({
+        "consume_time_gt" : 0,
+        "consume_time_lte" : 12,
+        #打折参考的基准价格,默认等于该包厢类别room_type的钟点费,用户可以修改
+        "base_hourly_fee" : 0,
+        })
+
+    #包厢类别发生变化,基准价格同样改变
+    def onchange_room_type_id(self,cr,uid,ids,room_type_id):
+        if not room_type_id:
+            return {}
+        room_type = self.pool.get('ktv.ktv_room_type').browse(cr, uid, room_type_id)
+        vals = _reset_discount_info(room_type.hourly_fee)
+        vals.update({"base_hourly_fee" : room_type.hourly_fee})
+        return {"value" : vals}
+
+    #重新设置各天费用为100%
+    def _reset_discount_info(self,base_hourly_fee = 0):
+        vals = {field_name : base_hourly_fee for field_name in _fee_fields if field_name.find("fee") != -1}
+        vals.update({field_name : 100 for field_name in _fee_fields if field_name.find("discount") != -1})
+        return vals
+
+    #基准钟点费发生变换时,充值所有打折数据
+    def onchange_base_hourly_fee(self,cr,uid,ids,base_hourly_fee):
+        if base_hourly_fee and base_hourly_fee == 0 :
+            return {}
+        vals = _reset_discount_info(base_hourly_fee)
+        return {"values" : vals}
+
+    #钟点费或钟点费折扣互为变化
+    #context中传递了变化的字段
+    def onchange_hourly_fee(self,cr,uid,ids,field_name = False,hourly_fee_or_discount_rate = False ,base_hourly_fee = False,context = None):
+        val ={}
+        if not base_hourly_fee or not field_name or field_name.find("continue_hourly_fee") != -1 :
+            return {}
+        #如果是修改钟点费,则自动计算折扣百分比
+        if field_name.find("hourly_fee") != -1 :
+            to_change_fieldname = field_name.replace('fee','discount')
+            val[to_change_fieldname] = hourly_fee_or_discount_rate / base_hourly_fee * 100
+
+        #如果是修改钟点费折扣,则自动计算折扣后的钟点费
+        if field_name.find("hourly_discount") != -1 :
+            to_change_fieldname = field_name.replace('discount','fee')
+            val[to_change_fieldname] = base_hourly_fee * hourly_fee_or_discount_rate / 100
+
+        return {"values" : val}
+
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+
